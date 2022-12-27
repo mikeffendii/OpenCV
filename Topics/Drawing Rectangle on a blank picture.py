ab1b4b6047a5cd1ad5ccec7274df42862b70b4e5
Muhammad Ismail Khan Effendi <91178270+mikeffendii@users.noreply.github.com>
2022-12-27 16:24:47 +0500

    Create Drawing Rectangle on a blank picture.py

---
+++ Topics/Drawing Rectangle on a blank picture.py
@@ -0,0 +1,9 @@
+import cv2 as cv
+import numpy as np
+
+blank = np.zeros((500,500,3), dtype='uint8')
+
+cv.rectangle(blank, (0,0), (250,250), (0,255,0), thickness=2)
+
+cv.imshow("Blank Image", blank)
+cv.waitKey(0)
